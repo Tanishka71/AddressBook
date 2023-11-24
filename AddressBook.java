a1fc3c0abcfdfe51acb0c7b38f5165db4e51a3aa
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-24 12:27:37 +0530

    First commit.

---
+++ AddressBook.java
@@ -0,0 +1,15 @@
+package com.address;
+
+/*@desc:the base class originally*/
+public class AddressBook {
+	
+	
+	/*
+	 * @desc:Displays the message to welcome its users
+	 * @params:none
+	 * @retrun:none
+	 */
+	public static void main(String []args) {
+		System.out.println("WELCOME TO ADDRESS BOOK!!");
+	}
+}
